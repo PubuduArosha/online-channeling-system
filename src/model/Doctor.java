0c1bf3f6e1163a9bcb01f21d446fa2646c71d071
chanu_16 <chanuri16@gmail.com>
2020-04-12 02:05:23 +0530

    Implemented setters and getters in the doctor model

--- src/model/Doctor.java
+++ src/model/Doctor.java
@@ -2,5 +2,95 @@ package model;
 
 
 public class Doctor {
-
+	private int doctorID;
+	private String NIC;
+	private String gender;
+	private String fastName;
+	private String lastname;
+	private String email;
+	private String specification;
+	private int contact;
+	private String workDate;
+	private String workTime;
+	private String password;
+	private int doctorStatus;
+	
+	
+	public int getDoctorID() {
+		return doctorID;
+	}
+	public void setDoctorID(int doctorID) {
+		this.doctorID = doctorID;
+	}
+	
+	public String getNIC() {
+		return NIC;
+	}
+	public void setNIC(String nIC) {
+		NIC = nIC;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getFastName() {
+		return fastName;
+	}
+	public void setFastName(String fastName) {
+		this.fastName = fastName;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSpecification() {
+		return specification;
+	}
+	public void setSpecification(String specification) {
+		this.specification = specification;
+	}
+	public int getContact() {
+		return contact;
+	}
+	public void setContact(int contact) {
+		this.contact = contact;
+	}
+	public String getWorkDate() {
+		return workDate;
+	}
+	public void setWorkDate(String workDate) {
+		this.workDate = workDate;
+	}
+	public String getWorkTime() {
+		return workTime;
+	}
+	public void setWorkTime(String workTime) {
+		this.workTime = workTime;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getDoctorStatus() {
+		return doctorStatus;
+	}
+	public void setDoctorStatus(int doctorStatus) {
+		this.doctorStatus = doctorStatus;
+	}
+	
+	
+	
+	
 }
