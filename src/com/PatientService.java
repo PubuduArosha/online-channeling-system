a7f7ad219581e0448ab0e95ed06fa7db75be24b9
maaliWageesha <malidi26@yahoo.com>
2020-04-11 12:30:09 +0530

    I added configurations and merge with master branch.

---
+++ src/com/PatientService.java
@@ -0,0 +1,14 @@
+package com;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("patient")
+public class PatientService {
+	@GET 
+	@Produces("text/plain")
+    public String getIt() {
+        return "Hi Malit !";
+    }
+}
