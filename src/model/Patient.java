ab53ac8613d21ba0e6ef4c6a5071f9a92c320a23
maaliWageesha <malidi26@yahoo.com>
2020-04-11 19:57:47 +0530

    Added Model class patient

--- src/model/Patient.java
+++ src/model/Patient.java
@@ -1,5 +1,116 @@
 package model;
 
+/**
+ * @author Malidi
+ *
+ */
 public class Patient {
+	
+	
+	private int patientID;
+	
+	private String NIC;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
+	
+	private String address;
+	
+	private String password;
+	
+	private String city;
+	
+	private String contact;
+
+	public int getPatientID() {
+		return patientID;
+	}
+
+	public void setPatientID(int patientID) {
+		this.patientID = patientID;
+	}
+
+	public String getNIC() {
+		return NIC;
+	}
+
+	public void setNIC(String nIC) {
+		NIC = nIC;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	@Override
+	public String toString() {
+		return "Patient [patientID=" + patientID + ", NIC=" + NIC + ", firstName=" + firstName + ", lastName="
+				+ lastName + ", email=" + email + ", address=" + address + ", password=" + password + ", city=" + city
+				+ ", contact=" + contact + "]";
+	}
+
+	
+	
+	
+	
+	
+
+	
+	
 
 }
